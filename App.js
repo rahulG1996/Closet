1c94436dc17e8c520d0e04e867bedbb873817882
Rahul <rahul@Rahuls-MacBook-Pro.local>
2022-09-24 19:13:28 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+
+class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+  render() {
+    return null;
+  }
+}
+
+export default App;
