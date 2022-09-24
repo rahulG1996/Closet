61c49dbad6d1e4525d883545572ccb66b5b92e9e
Rahul <rahul@Rahuls-MacBook-Pro.local>
2022-09-24 23:13:12 +0530

    tab bar customise

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -1,11 +1,54 @@
 import Home from '../Screens/Home';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Text, Image} from 'react-native';
+import React from 'react';
+import {VText, VView} from '../components';
 
 const Tab = createBottomTabNavigator();
 
+const renderTab = (route, imgSource) => {
+  return (
+    <VView
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}>
+      <Image
+        source={imgSource}
+        style={{width: 24, height: 24}}
+        resizeMode="contain"
+      />
+      <VText text={route.name} style={{paddingLeft: 8}} />
+    </VView>
+  );
+};
+
 export function TabData() {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused}) => {
+          let imgSource = '';
+          if (route.name === 'Shop') {
+            imgSource = focused
+              ? require('../assets/iLockSelected.webp')
+              : require('../assets/iLock.webp');
+          } else if (route.name === 'Closet') {
+            imgSource = focused
+              ? require('../assets/iClosetSelected.webp')
+              : require('../assets/iCloset.webp');
+          }
+          if (route.name === 'Outfits') {
+            imgSource = focused
+              ? require('../assets/iOutfitSelected.webp')
+              : require('../assets/outfite.webp');
+          }
+
+          return renderTab(route, imgSource);
+        },
+        tabBarShowLabel: false,
+        headerShown: false,
+      })}>
       <Tab.Screen name="Shop" component={Home} />
       <Tab.Screen name="Closet" component={Home} />
       <Tab.Screen name="Outfits" component={Home} />
